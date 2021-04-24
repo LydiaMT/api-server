303d5673b5a7eff7241aba93a523df71de61b2b9
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-04-23 19:49:23 -0700

    atlas issues

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: 'foodclothes'
 };
 
 mongoose.connect(process.env.MONGODB_URI, options)
