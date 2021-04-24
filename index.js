42006c7e05e2ba10b0d65284ebc3aeacc2ad1b1b
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-04-23 18:49:16 -0700

    mongo db edits

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 'use strict';
 
+require('dotenv').config();
 const mongoose = require('mongoose');
-const { default: MongoMemoryServer } = require('mongodb-memory-server');
-module.exports = require('supertest');
-const mongoServer = new MongoMemoryServer();
-
-
-// const DataCollection = require('./src/models/data-collection-class.js');
-// const food = new DataCollection();
-// const clothes = new DataCollection();
-
 const server = require('./src/server');
 const PORT = process.env.PORT || 3000;
+
 // const MONGODB_URI = 'mongodb://localhost:27017/foodclothes';
 // mongoose.connect(process.env.MONGO_URI, options);
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  poolSize: 10
 };
 
-mongoServer.getConnectionString()
-  .then(mongoUri => {
-    mongoose.connect(mongoUri, process.env.MONGO_URI, options);
-    server.start(PORT);
-  });
+mongoose.connect(process.env.MONGODB_URI, options);
+server.start(PORT);
--- src/server.js
+++ src/server.js
@@ -4,15 +4,14 @@ const express = require('express');
 const app = express();
 
 const logger = require('./middleware/logger');
-const { clothesRouter }= require('./routes/clothes.js');
 const { foodRouter } = require('./routes/food.js');
-
+const { clothesRouter }= require('./routes/clothes.js');
 const notFound =require('./error-handlers/404.js');
 const errors = require('./error-handlers/500.js');
 
 app.use(express.json());
-
 app.use(logger);
+
 app.use(clothesRouter);
 app.use(foodRouter);
 
